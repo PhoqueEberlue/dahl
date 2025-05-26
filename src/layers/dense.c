5843a23dfebbdd3cea0307c7c92024e544fba432
PhoqueEberlue <andrewmhdb@gmail.com>
2025-05-26 16:43:18 +0200

    working on the parallelization of the dense layer

--- src/layers/dense.c
+++ src/layers/dense.c
@@ -8,9 +8,9 @@ dahl_dense* dense_init(dahl_shape3d const input_shape, size_t const output_size)
     *(dahl_shape3d*)&dense->input_shape = input_shape;
     *(size_t*)&dense->output_size = output_size;
 
-    dahl_shape2d weights_shape = { .x = input_shape.x * input_shape.y * input_shape.z, .y = output_size };
+    dahl_shape3d weights_shape = { .x = input_shape.x * input_shape.y, .y = output_size, .z = input_shape.z };
 
-    dense->weights =  matrix_init_random(weights_shape);
+    dense->weights =  block_init_random(weights_shape);
     dense->biases =  vector_init_random(output_size);
 
     return dense;
@@ -18,14 +18,41 @@ dahl_dense* dense_init(dahl_shape3d const input_shape, size_t const output_size)
 
 dahl_vector* dense_forward(dahl_dense* dense, dahl_block* input_data)
 {
-    // TODO: here it deletes the original input_data pointer which makes it unavailable for the caller, not good right?
-    dense->input_data_flattened = block_to_vector(input_data);
-    dahl_vector* tmp = task_matrix_vector_product_init(dense->weights, dense->input_data_flattened);
-    TASK_ADD_SELF(tmp, dense->biases);
+    dense->input_data = input_data;
 
-    dense->output = task_vector_softmax_init(tmp);
+    // x: number of classes, y: number of channels
+    dahl_shape2d tmp_shape = { .x = dense->output_size, .y = dense->input_shape.z };
 
-    vector_finalize(tmp);
+    dahl_matrix* tmp = matrix_init(tmp_shape);
+
+    block_partition_along_z(input_data);
+    block_partition_along_z(dense->weights);
+    matrix_partition_along_y(tmp);
+
+    size_t const n_channels = block_get_sub_matrix_nb(input_data);
+
+    for (size_t i = 0; i < n_channels; i++)
+    {
+        dahl_matrix const* sub_input = block_get_sub_matrix(input_data, i);
+        dahl_matrix const* sub_weights = block_get_sub_matrix(dense->weights, i);
+        dahl_vector* sub_tmp = matrix_get_sub_vector(tmp, i);
+        dahl_vector const* sub_input_flatten = matrix_as_vector(sub_input);
+
+        task_matrix_vector_product(sub_weights, sub_input_flatten, sub_tmp);
+    }
+
+    block_unpartition(input_data);
+    block_unpartition(dense->weights);
+    matrix_unpartition(tmp);
+
+    dahl_vector* out = task_matrix_sum_y_axis(tmp);
+
+    TASK_ADD_SELF(out, dense->biases);
+
+    dense->output = task_vector_softmax_init(out);
+
+    matrix_finalize(tmp);
+    vector_finalize(out);
 
     return dense->output;
 }
@@ -34,30 +61,54 @@ dahl_block* dense_backward(dahl_dense* dense, dahl_vector const* dl_dout, dahl_f
 {
     dahl_matrix const* tmp = task_vector_softmax_derivative(dense->output);
 
-    dahl_vector const* dl_dy = task_matrix_vector_product_init(tmp, dl_dout);
-
-    dahl_vector* dl_dy_clone = vector_clone(dl_dy);
+    dahl_vector* dl_dy = task_matrix_vector_product_init(tmp, dl_dout);
 
     // We need to clone dl_dy because its dissapearing here
-    dahl_matrix* dl_dy_mat = vector_to_column_matrix(dl_dy_clone);
-    // FIX Here it deletes the data pointed by dense->input_data_flattened, which is *fine* for this code but not good in general
-    dahl_matrix* input_mat = vector_to_row_matrix(dense->input_data_flattened);
+    dahl_matrix const* dl_dy_col = vector_as_column_matrix(dl_dy);
 
-    dahl_matrix* dl_dw = task_matrix_matrix_product_init(dl_dy_mat, input_mat);
+    dahl_shape3d dl_dw_shape = { .x = dense->input_shape.x * dense->input_shape.y, .y = dense->output_size, .z = dense->input_shape.z };
+    dahl_block* dl_dw = block_init(dl_dw_shape);
 
-    dahl_matrix* weights_t = task_matrix_transpose_init(dense->weights);
+    // Backward return an output of the same shape of the forward input
+    dahl_block* output = block_init(dense->input_shape);
 
-    dahl_vector* dl_dinput = task_matrix_vector_product_init(weights_t, dl_dy);
+    block_partition_along_z(dense->input_data);
+    block_partition_along_z(dense->weights);
+    block_partition_along_z(dl_dw);
+    block_partition_along_z(output);
 
-    dahl_block* dl_dinput_block = vector_to_block(dl_dinput, dense->input_shape);
+    size_t const n_channels = block_get_sub_matrix_nb(dense->input_data);
 
-    TASK_SCAL_SELF(dl_dw, learning_rate);
+    for (size_t i = 0; i < n_channels; i++)
+    {
+        dahl_matrix const* sub_input = block_get_sub_matrix(dense->input_data, i);
+        dahl_vector const* sub_input_flatten = matrix_as_vector(sub_input);
+        dahl_matrix const* sub_input_flatten_row = vector_as_row_matrix(sub_input_flatten);
 
-    dahl_vector* tmp_2 = vector_init(vector_get_len(dl_dy));
-    TASK_SCAL(dl_dy, tmp_2, learning_rate);
+        dahl_matrix* sub_dl_dw = block_get_sub_matrix(dl_dw, i);
 
-    TASK_SUB_SELF(dense->weights, dl_dw);
-    TASK_SUB_SELF(dense->biases, tmp_2);
+        task_matrix_matrix_product(dl_dy_col, sub_input_flatten_row, sub_dl_dw);
 
-    return dl_dinput_block;
+        dahl_matrix* sub_weights = block_get_sub_matrix(dense->weights, i);
+        dahl_matrix const* sub_weights_t = task_matrix_transpose_init(sub_weights);
+
+        dahl_matrix* sub_output = block_get_sub_matrix(output, i);
+        dahl_vector* sub_output_vec = matrix_as_vector(sub_output); // Get a view as a vector so we can write the product output
+        task_matrix_vector_product(sub_weights_t, dl_dy, sub_output_vec);
+
+        // Updating weights
+        TASK_SCAL_SELF(sub_dl_dw, learning_rate);
+        TASK_SUB_SELF(sub_weights, sub_dl_dw);
+    }
+
+    block_unpartition(dense->input_data);
+    block_unpartition(dense->weights);
+    block_unpartition(dl_dw);
+    block_unpartition(output);
+
+    // Updating biases
+    TASK_SCAL_SELF(dl_dy, learning_rate);
+    TASK_SUB_SELF(dense->biases, dl_dy);
+
+    return output;
 }
