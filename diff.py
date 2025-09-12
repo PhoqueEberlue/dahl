61943f16b5201e59b35bec530cb360385b2081a8
PhoqueEberlue <andrewmhdb@gmail.com>
2025-09-12 17:21:59 +0200

    showing less lines in the diff script

--- diff.py
+++ diff.py
@@ -2,11 +2,13 @@ import sys
 from termcolor import colored, cprint
 
 with open(sys.argv[1]) as f1, open(sys.argv[2]) as f2:
+    line_count = 0
     for a, b in zip(f1, f2):
         la, lb = a.rstrip("\n"), b.rstrip("\n")
         # if the two lines matches, simply print in white
         if la == lb:
             print(la, end="")
+            line_count += 1
         else:
             # otherwise print the missmatching line two times,
             # one in green with the differing character highlighted
@@ -22,4 +24,8 @@ with open(sys.argv[1]) as f1, open(sys.argv[2]) as f2:
                     cprint(ca, "red", end="")
                 else:
                     cprint(cb, "black", "on_red", end="")
+            line_count += 2
         print()
+        if line_count > 10:
+            print("...")
+            break
