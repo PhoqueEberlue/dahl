afb65b29ce7790ef9fcbd193d96244d9f8f4b0ae
PhoqueEberlue <andrewmhdb@gmail.com>
2025-09-16 14:43:28 +0200

    Now displaying images with imagemagick instead of viewing in the terminal :)

--- src/learning/utils.c
+++ src/learning/utils.c
@@ -1,17 +1,16 @@
 #include "../../include/dahl_utils.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void print_predictions_batch(dahl_matrix const* predictions_batch, dahl_matrix const* target_batch, 
         dahl_tensor const* image_batch, char const** labels)
 {
-    matrix_print(predictions_batch);
-    matrix_print(target_batch);
-
-    dahl_fp const* preds = matrix_data_acquire(predictions_batch);
-    dahl_fp const* targs = matrix_data_acquire(target_batch);
+    matrix_acquire(predictions_batch);
+    matrix_acquire(target_batch);
 
     size_t const batch_size = matrix_get_shape(predictions_batch).y;
     size_t const num_classes = matrix_get_shape(predictions_batch).x;
+    size_t const num_channels = tensor_get_shape(image_batch).z;
 
     tensor_partition_along_t(image_batch);
 
@@ -20,18 +19,20 @@ void print_predictions_batch(dahl_matrix const* predictions_batch, dahl_matrix c
         dahl_block const* image = GET_SUB_BLOCK(image_batch, y);
 
         size_t max_index = 0;
-        // FIXME: really need to improve indexing of data structures, here I do (y*ld) but it's not obvious at all.
-        dahl_fp max_value = preds[y*num_classes];
         size_t true_index = 0;
+        dahl_fp max_value = matrix_get_value(predictions_batch, max_index, y);
 
         for (size_t x = 0; x < num_classes; x++)
         {
-           if (preds[(y*num_classes)+x] > max_value) 
+           dahl_fp pred_val = matrix_get_value(predictions_batch, x, y);
+           dahl_fp targ_val = matrix_get_value(target_batch, x, y);
+
+           if (pred_val > max_value) 
            {
                max_index = x;
-               max_value = preds[(y*num_classes)+x];
+               max_value = pred_val;
            }
-           if (targs[(y*num_classes)+x] == 1.0F) 
+           if (targ_val == 1.0F) 
            {
                true_index = x;
            }
@@ -40,16 +41,30 @@ void print_predictions_batch(dahl_matrix const* predictions_batch, dahl_matrix c
         printf("Predicted class: %s (%lu)\n", labels[max_index], max_index);
         printf("True class: %s (%lu)\n", labels[true_index], true_index);
 
-        // Partition by image channel dimension, and only display one channel for now
-        // TODO: sum the colors channels to display in the terminal in the future?
-        block_partition_along_z(image);
-        dahl_matrix const* image_single_channel = GET_SUB_MATRIX(image, 0);
-        matrix_print_ascii(image_single_channel, 0.5);
-        block_unpartition(image);
+        // Display using ImageMagick
+        if (num_channels == 1)
+        {
+            block_partition_along_z(image);
+            dahl_matrix const* image_single_channel = GET_SUB_MATRIX(image, 0);
+            matrix_image_display(image_single_channel, 10);
+
+            // We can also display in ASCII in the terminal
+            // matrix_print_ascii(image_single_channel, 0.5);
+            block_unpartition(image);
+        }
+        else if (num_channels == 3)
+        {
+            block_image_display(image, 10);
+        }
+        else
+        {
+            printf("Error, wrong number of channels");
+            exit(1);
+        } 
     }
 
     tensor_unpartition(image_batch);
 
-    matrix_data_release(predictions_batch);
-    matrix_data_release(target_batch);
+    matrix_release(predictions_batch);
+    matrix_release(target_batch);
 }
--- src/types.c
+++ src/types.c
@@ -1,6 +1,7 @@
 #include "../include/dahl_types.h"
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 bool shape2d_equals(dahl_shape2d const a, dahl_shape2d const b)
 {
@@ -36,4 +37,23 @@ dahl_fp fp_round(dahl_fp const value, u_int8_t const precision)
 {
     dahl_fp power = pow(10.0F, precision);
     return round(value * power) / power;
+}
+
+bool fp_equals(dahl_fp a, dahl_fp b)
+{
+    return (bool)(a == b);
+}
+
+bool fp_equals_round(dahl_fp a, dahl_fp b, u_int8_t precision)
+{
+    return (bool)(fp_round(a, precision) == fp_round(b, precision));
+}
+
+dahl_fp fp_rand(dahl_fp min, dahl_fp max)
+{
+    // Generate a random number between 0 and 1
+    dahl_fp rand_norm = (dahl_fp)rand() / (dahl_fp)(RAND_MAX);
+    // Apply the range
+    dahl_fp rand_val = ((rand_norm * (max - min)) + min);
+    return rand_val;
 }
