cdcb4a86dbbcdf97d76650c9b4c4cb3af43db0ab
PhoqueEberlue <andrewmhdb@gmail.com>
2025-11-13 15:12:52 +0000

    now printing when test passes

--- dahl/src/asserts.c
+++ dahl/src/asserts.c
@@ -14,9 +14,14 @@
 #define ANSI_COLOR_RESET   "\x1b[0m"
 #define ANSI_COLOR_FLUSH   "\33[0m"
 
-void log_prefix(char const* file, int const line, char const* function)
+void log_prefix_fail(char const* file, int const line, char const* function)
 {
-    printf("[DAHL][FAIL][%s:%s:%d] ", file, function, line);
+    printf("[DAHL][TESTS][" ANSI_COLOR_RED "FAIL" ANSI_COLOR_FLUSH "][%s:%s:%d] ", file, function, line);
+}
+
+void log_prefix_ok(char const* file, int const line, char const* function)
+{
+    printf("[DAHL][TESTS][" ANSI_COLOR_GREEN "OK" ANSI_COLOR_FLUSH "][%s]\n", function);
 }
 
 void print_diff(void const* a, void const* b, char const* a_expr, char const* b_expr, int8_t precision, dahl_traits* traits)
@@ -58,10 +63,14 @@ void assert_scalar_equals(dahl_scalar const* a, dahl_scalar const* b,
 {
     if (!scalar_equals(a, b, rounding, precision))
     {
-        log_prefix(file, line, function);
+        log_prefix_fail(file, line, function);
         printf("Assert scalar equals: " ANSI_COLOR_GREEN "%s" ANSI_COLOR_FLUSH " != " ANSI_COLOR_RED "%s" ANSI_COLOR_FLUSH "\n", a_expr, b_expr);
         PRINT_DIFF_EXPR(a, b, a_expr, b_expr, precision);
         printf("\n");
+    }
+    else
+    {
+        log_prefix_ok(file, line, function);
     }
 }
 
@@ -72,10 +81,14 @@ void assert_vector_equals(dahl_vector const* a, dahl_vector const* b,
 {
     if (!vector_equals(a, b, rounding, precision))
     {
-        log_prefix(file, line, function);
+        log_prefix_fail(file, line, function);
         printf("Assert vector equals: " ANSI_COLOR_GREEN "%s" ANSI_COLOR_FLUSH " != " ANSI_COLOR_RED "%s" ANSI_COLOR_FLUSH "\n", a_expr, b_expr);
         PRINT_DIFF_EXPR(a, b, a_expr, b_expr, precision);
         printf("\n");
+    }
+    else
+    {
+        log_prefix_ok(file, line, function);
     }
 }
 
@@ -86,10 +99,14 @@ void assert_matrix_equals(dahl_matrix const* a, dahl_matrix const* b,
 {
     if (!matrix_equals(a, b, rounding, precision))
     {
-        log_prefix(file, line, function);
+        log_prefix_fail(file, line, function);
         printf("Assert matrix equals: " ANSI_COLOR_GREEN "%s" ANSI_COLOR_FLUSH " != " ANSI_COLOR_RED "%s" ANSI_COLOR_FLUSH "\n", a_expr, b_expr);
         PRINT_DIFF_EXPR(a, b, a_expr, b_expr, precision);
         printf("\n");
+    }
+    else
+    {
+        log_prefix_ok(file, line, function);
     }
 }
 
@@ -100,10 +117,14 @@ void assert_block_equals(dahl_block const* a, dahl_block const* b,
 {
     if (!block_equals(a, b, rounding, precision))
     {
-        log_prefix(file, line, function);
+        log_prefix_fail(file, line, function);
         printf("Assert block equals: " ANSI_COLOR_GREEN "%s" ANSI_COLOR_FLUSH " != " ANSI_COLOR_RED "%s" ANSI_COLOR_FLUSH "\n", a_expr, b_expr);
         PRINT_DIFF_EXPR(a, b, a_expr, b_expr, precision);
         printf("\n");
+    }
+    else
+    {
+        log_prefix_ok(file, line, function);
     }
 }
 
@@ -114,10 +135,14 @@ void assert_tensor_equals(dahl_tensor const* a, dahl_tensor const* b,
 {
     if (!tensor_equals(a, b, rounding, precision))
     {
-        log_prefix(file, line, function);
+        log_prefix_fail(file, line, function);
         printf("Assert tensor equals: " ANSI_COLOR_GREEN "%s" ANSI_COLOR_FLUSH " != " ANSI_COLOR_RED "%s" ANSI_COLOR_FLUSH "\n", a_expr, b_expr);
         PRINT_DIFF_EXPR(a, b, a_expr, b_expr, precision);
         printf("\n");
+    }
+    else
+    {
+        log_prefix_ok(file, line, function);
     }
 }
 
@@ -139,11 +164,15 @@ void assert_fp_equals(dahl_fp const a, dahl_fp const b,
     
     if (!match)
     {
-        log_prefix(file, line, function);
+        log_prefix_fail(file, line, function);
         printf("Assert dahl_fp equals: %s != %s\n", a_expr, b_expr);
         printf("%s = %+.15f\n", a_expr, a);
         printf("%s = %+.15f\n", b_expr, b);
         printf("\n");
+    }
+    else
+    {
+        log_prefix_ok(file, line, function);
     }
 }
 
@@ -153,11 +182,15 @@ void assert_size_t_equals(size_t const a, size_t const b,
 {
     if (a != b)
     {
-        log_prefix(file, line, function);
+        log_prefix_fail(file, line, function);
         printf("Assert size_t equals: %s != %s\n", a_expr, b_expr);
         printf("%s = %zu\n", a_expr, a);
         printf("%s = %zu\n", b_expr, b);
         printf("\n");
+    }
+    else
+    {
+        log_prefix_ok(file, line, function);
     }
 }
 
@@ -167,13 +200,17 @@ void assert_shape2d_equals(dahl_shape2d const a, dahl_shape2d const b,
 {
     if(!shape2d_equals(a , b))
     {
-        log_prefix(file, line, function);
+        log_prefix_fail(file, line, function);
         printf("Assert shape2d equals: %s != %s\n", a_expr, b_expr);
         printf("%s: ", a_expr);
         shape2d_print(a);
         printf("%s: ", b_expr);
         shape2d_print(b);
         printf("\n");
+    }
+    else
+    {
+        log_prefix_ok(file, line, function);
     }
 }
 
@@ -183,13 +220,17 @@ void assert_shape3d_equals(dahl_shape3d const a, dahl_shape3d const b,
 {
     if(!shape3d_equals(a , b))
     {
-        log_prefix(file, line, function);
+        log_prefix_fail(file, line, function);
         printf("Assert shape3d equals: %s != %s\n", a_expr, b_expr);
         printf("%s: ", a_expr);
         shape3d_print(a);
         printf("%s: ", b_expr);
         shape3d_print(b);
         printf("\n");
+    }
+    else
+    {
+        log_prefix_ok(file, line, function);
     }
 }
 
@@ -199,12 +240,16 @@ void assert_shape4d_equals(dahl_shape4d const a, dahl_shape4d const b,
 {
     if(!shape4d_equals(a , b))
     {
-        log_prefix(file, line, function);
+        log_prefix_fail(file, line, function);
         printf("Assert shape3d equals: %s != %s\n", a_expr, b_expr);
         printf("%s: ", a_expr);
         shape4d_print(a);
         printf("%s: ", b_expr);
         shape4d_print(b);
         printf("\n");
+    }
+    else
+    {
+        log_prefix_ok(file, line, function);
     }
 }
